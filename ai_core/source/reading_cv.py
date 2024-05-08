8686e22c2da6345551485a2cde3212862f282c5a
binhle2001 <minhan012710@gmail.com>
2024-05-08 14:22:37 +0700

    first-commit

--- ai_core/source/reading_cv.py
+++ ai_core/source/reading_cv.py
@@ -3,18 +3,16 @@ from fastapi import FastAPI, File, UploadFile, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import io
-import google.generativeai as genai
 from PyPDF2 import PdfReader
 import os 
 import json
-from google.generativeai.types import HarmCategory, HarmBlockThreshold
-from ai_core.source.getting_email import get_attachment
+from openai import OpenAI
 from api.helpers.common import get_env_var
 from api.helpers.database import get_db
 from api.schema import ProcessStatus
 
-genai.configure(api_key = get_env_var("gemini", "API_KEY"))
-model_gemini = genai.GenerativeModel('gemini-pro')
+
+client = OpenAI(api_key=get_env_var("openai", "API_KEY"))
 app = FastAPI()
 
 
@@ -128,42 +126,27 @@ def extract_candidate_info(contents):
             "http_code": status.HTTP_400_BAD_REQUEST,
             "message": "Định dạng file không hỗ trợ"
         }
-    prompt = f"""
-            <|im_start|> DOCUMENT
-            {pdf_info.text}
-            <|im_end|>
-            <|im_start|> system
-            Tokyo Tech Lab Bot is a bot designed to extract information from the content of PDF files. Tokyo Tech Lab Bot is capable of:
-            - Please rewrite this with correct spelling. For example: "Đ ạ i h ọ c Qu ố c Gia Hà N ộ i" is changed to "Đại học Quốc Gia Hà Nội".
-            - Extracting information such as name, position applied for, phone number, email, work experience/projects undertaken, education, skills.
-            - Providing responses in the form of a dictionary as follows:
-            {info_format}
-            - Do not use external knowledge beyond the information provided in the DOCUMENT. If information is not provided, write "null". If a field has no information, return the entire field as null.
-            <|im_end|>
-            <|im_start|> assistant
-            """.strip()
-    output = model_gemini.generate_content(
-        prompt,
-        generation_config=genai.types.GenerationConfig(
-                                    candidate_count = 1,
-                                    stop_sequences = ['<|im_end|>'],
-                                    max_output_tokens = 2000,
-                                    top_p = 0.9,
-                                    top_k = 7,
-                                    temperature = 0.1),
-        safety_settings={
-            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-        },
-
-    )
+    model="gpt-3.5-turbo"
+    SYSTEM_CONTENT = f"""Tokyo Tech Lab Bot is a bot designed to extract information from the content of PDF files. Tokyo Tech Lab Bot is capable of:
+        - Please rewrite this with correct spelling. For example: "Đ ạ i h ọ c Qu ố c Gia Hà N ộ i" is changed to "Đại học Quốc Gia Hà Nội".
+        - Extracting information such as name, position applied for, email, work experience/projects undertaken, education, skills.
+        - Providing responses in the form of a dictionary as follows:
+        {info_format}
+        - Do not use external knowledge beyond the information provided in the PDF files. If information is not provided, write "null". If a field has no information, return the entire field as null."""
+    messages = [
+            {"role": "system", "content": SYSTEM_CONTENT},
+            {"role": "user", "content": "The PDF file content is "+ pdf_info.text},
+        ]
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages= messages
+)
+    output = response.choices[0].message.content
     try:
-        parse_data = json.loads(output.text.replace("\n", ""))
+        parse_data = json.loads(output.replace("\n", ""))
         return {
             "http_code": status.HTTP_200_OK,
-            "message": str(e),
+            "data": parse_data,
         }
             
     except Exception as e:
